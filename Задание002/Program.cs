43a6bafb97cbdebce4e8a60e9ab3c3e188333d34
bloodymedley <de4ster@gmail.com>
2023-08-08 19:42:38 +0300

    Задание2

---
+++ Задание002/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b) {
+    if (a>c){
+        Console.WriteLine("Максимальное число " + a);
+    }
+}
+if (b>a){
+    if (b>c){
+        Console.WriteLine("Максимальное число " + b);
+    }
+}
+if (c>a){
+    if (c>b){
+        Console.WriteLine("Максимальное число " + c);
+    }
+}
+else {
+        Console.WriteLine("Числа равны");
+    }
